60ad18e6f6731d94d10039186eff242e1c9aae25
eko-develops <ekodevelops@gmail.com>
2021-11-12 12:12:06 -0800

    Style buttons

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       </header>
       <div className="content">
         <h1>Pomodoro Clock</h1>
-        <h2 className="time">00:00</h2>
+        <h2 className="time">25:00</h2>
         <div className="button-wrapper">
           <button type="button" className="start-button">Start</button>
           <button type="button" className="pause-button">Pause</button>
