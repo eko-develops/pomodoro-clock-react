757ba7d69289bbaee1dc99790a306f20c8085e41
eko-develops <ekodevelops@gmail.com>
2021-11-12 12:04:33 -0800

    Front-end styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 function App() {
   return (
     <div className="App">
+      <header>
+        <div className="brand">
+          ekodevelops
+        </div>
+        <div className="socials">
+          <div className="single-social">
+            <div className="social-icon">
+            </div>
+            <span>GitHub</span>
+          </div>
+        </div>
+      </header>
       <div className="content">
         <h1>Pomodoro Clock</h1>
-        <h2>00:00</h2>
+        <h2 className="time">00:00</h2>
         <div className="button-wrapper">
           <button type="button" className="start-button">Start</button>
           <button type="button" className="pause-button">Pause</button>
