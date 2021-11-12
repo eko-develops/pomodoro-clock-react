89cb9a4be40f7fbaea8209f94769e3b618a19458
eko-develops <ekodevelops@gmail.com>
2021-11-12 12:25:13 -0800

    Add test functions for buttons onClick

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,12 +1,23 @@
 const Clock = () => {
+
+    const handleStart = () => {
+        console.log('start clicked');
+    }
+    const handlePause = () => {
+        console.log('pause clicked');
+    }
+    const handleReset = () => {
+        console.log('reset clicked');
+    }
+
     return (
         <>
         <h1>Pomodoro Clock</h1>
         <h2 className="time">25:00</h2>
         <div className="button-wrapper">
-          <button type="button" className="start-button">Start</button>
-          <button type="button" className="pause-button">Pause</button>
-          <button type="button" className="reset-button">Reset</button>
+          <button onClick={handleStart} type="button" className="start-button">Start</button>
+          <button onClick={handlePause} type="button" className="pause-button">Pause</button>
+          <button onClick={handleReset} type="button" className="reset-button">Reset</button>
         </div>  
         </>
     )
