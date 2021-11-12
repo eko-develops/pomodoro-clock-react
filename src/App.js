70ecd28239e2a37eaee9b0b16484a458be25181c
eko-develops <ekodevelops@gmail.com>
2021-11-12 11:32:41 -0800

    Add main components and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+      <div className="content">
+        <h1>Pomodoro Clock</h1>
+        <h2>00:00</h2>
+        <div className="button-wrapper">
+          <button type="button" className="start-button">Start</button>
+          <button type="button" className="pause-button">Pause</button>
+          <button type="button" className="reset-button">Reset</button>
 
+        </div>
+      </div>
+    </div>
+      );
+}
 export default App;
