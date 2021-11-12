6e27592b6dd07df32ab00be4fae45c5d5f6ac4b3
eko-develops <ekodevelops@gmail.com>
2021-11-12 13:57:34 -0800

    Basic functionality of the clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,24 +1,67 @@
+import { useState, useEffect } from 'react';
+
 const Clock = () => {
+
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(3);
+    const [isRunning, setIsRunning] = useState(false);
+    const [isFinished, setIsFinished] = useState(false);
+
+    const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    useEffect(() => {
+        if(isRunning){
+        
+        const interval = setInterval( () => {
+            if(seconds === 0){
+                if(minutes !== 0){
+                    setSeconds(59);
+                    setMinutes(minutes - 1);
+                } else {
+                    //the timer is over
+                    setIsFinished(true);
+                    setIsRunning(false);
+                }
+            } else {
+                setSeconds(seconds - 1);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+            
+        }
+        
+    }, [isRunning, seconds, minutes]);
 
     const handleStart = () => {
         console.log('start clicked');
+        setIsRunning(true);
     }
     const handlePause = () => {
         console.log('pause clicked');
+        setIsRunning(false);
     }
     const handleReset = () => {
         console.log('reset clicked');
+        setIsRunning(false);
+        setIsFinished(false);
+        setMinutes(25);
+        setSeconds(0);
     }
 
     return (
         <>
         <h1>Pomodoro Clock</h1>
-        <h2 className="time">25:00</h2>
+        <h2 className="time">{timerMinutes}:{timerSeconds}</h2>
         <div className="button-wrapper">
           <button onClick={handleStart} type="button" className="start-button">Start</button>
           <button onClick={handlePause} type="button" className="pause-button">Pause</button>
           <button onClick={handleReset} type="button" className="reset-button">Reset</button>
         </div>  
+          <button type="button" className="settings-button">Settings</button>
+          {isRunning ? <div>running</div> : <div>not running</div>}
+          {isFinished ? <div>finished</div> : <div>not finished</div>}
         </>
     )
 }
