0730cba5900f078345679efc88a32be905f9df7f
eko-develops <ekodevelops@gmail.com>
2021-11-12 16:17:27 -0800

    Change what displays on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import { BsGithub } from 'react-icons/bs';
 const Header = () => {
     return (
         <header>
-            <div className="brand">
-            <span>eko-develops</span>
+            <div className="top-heading">
+            <span>Pomodoro Clock by ekoret</span>
             </div>
             <div className="socials">
             <div className="single-social">
                 <a target="_blank" href="https://github.com/eko-develops">
                     <BsGithub size={25} />
-                    <span>GitHub</span>
+                    <span>eko-develops</span>
                 </a>
             </div>
             </div>
